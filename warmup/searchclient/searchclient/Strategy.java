1af1713b9f856afc3997ed1bdc64ac118b57f87b
Elias Obeid <e.k.obeid@gmail.com>
2016-02-08 22:35:26 +0100

    added best first search implementation with priorityqueue

--- warmup/searchclient/searchclient/Strategy.java
+++ warmup/searchclient/searchclient/Strategy.java
@@ -3,157 +3,158 @@ package searchclient;
 import java.util.ArrayDeque;
 import java.util.HashSet;
 import java.util.Stack;
+import java.util.PriorityQueue;
 
 import searchclient.SearchClient.Memory;
 import searchclient.Node;
 
 public abstract class Strategy {
 
-	public HashSet< Node > explored;
-	public long startTime = System.currentTimeMillis();
+  public HashSet< Node > explored;
+  public long startTime = System.currentTimeMillis();
 
-	public Strategy() {
-		explored = new HashSet< Node >();
-	}
+  public Strategy() {
+    explored = new HashSet< Node >();
+  }
 
-	public void addToExplored( Node n ) {
-		explored.add( n );
-	}
+  public void addToExplored( Node n ) {
+    explored.add( n );
+  }
 
-	public boolean isExplored( Node n ) {
-		return explored.contains( n );
-	}
+  public boolean isExplored( Node n ) {
+    return explored.contains( n );
+  }
 
-	public int countExplored() {
-		return explored.size();
-	}
+  public int countExplored() {
+    return explored.size();
+  }
 
-	public String searchStatus() {
-		return String.format( "#Explored: %4d, #Frontier: %3d, Time: %3.2f s \t%s", countExplored(), countFrontier(), timeSpent(), Memory.stringRep() );
-	}
-	
-	public float timeSpent() {
-		return ( System.currentTimeMillis() - startTime ) / 1000f;
-	}
+  public String searchStatus() {
+    return String.format( "#Explored: %4d, #Frontier: %3d, Time: %3.2f s \t%s", countExplored(), countFrontier(), timeSpent(), Memory.stringRep() );
+  }
+  
+  public float timeSpent() {
+    return ( System.currentTimeMillis() - startTime ) / 1000f;
+  }
 
-	public abstract Node getAndRemoveLeaf();
+  public abstract Node getAndRemoveLeaf();
 
-	public abstract void addToFrontier( Node n );
+  public abstract void addToFrontier( Node n );
 
-	public abstract boolean inFrontier( Node n );
+  public abstract boolean inFrontier( Node n );
 
-	public abstract int countFrontier();
+  public abstract int countFrontier();
 
-	public abstract boolean frontierIsEmpty();
-	
-	public abstract String toString();
+  public abstract boolean frontierIsEmpty();
+  
+  public abstract String toString();
 
-	public static class StrategyBFS extends Strategy {
+  public static class StrategyBFS extends Strategy {
 
-		private ArrayDeque< Node > frontier;
+    private ArrayDeque< Node > frontier;
 
-		public StrategyBFS() {
-			super();
-			frontier = new ArrayDeque< Node >();
-		}
+    public StrategyBFS() {
+      super();
+      frontier = new ArrayDeque< Node >();
+    }
 
-		public Node getAndRemoveLeaf() {
-			return frontier.pollFirst();
-		}
+    public Node getAndRemoveLeaf() {
+      return frontier.pollFirst();
+    }
 
-		public void addToFrontier( Node n ) {
-			frontier.addLast( n );
-		}
+    public void addToFrontier( Node n ) {
+      frontier.addLast( n );
+    }
 
-		public int countFrontier() {
-			return frontier.size();
-		}
+    public int countFrontier() {
+      return frontier.size();
+    }
 
-		public boolean frontierIsEmpty() {
-			return frontier.isEmpty();
-		}
+    public boolean frontierIsEmpty() {
+      return frontier.isEmpty();
+    }
 
-		public boolean inFrontier( Node n ) {
-			return frontier.contains( n );
-		}
+    public boolean inFrontier( Node n ) {
+      return frontier.contains( n );
+    }
 
-		public String toString() {
-			return "Breadth-first Search";
-		}
-	}
+    public String toString() {
+      return "Breadth-first Search";
+    }
+  }
 
-	public static class StrategyDFS extends Strategy {
+  public static class StrategyDFS extends Strategy {
 
-		private Stack< Node > frontier;
+    private Stack< Node > frontier;
     private int stackSize = 0;
 
-		public StrategyDFS() {
-			super();
-			frontier = new Stack< Node >();
-		}
+    public StrategyDFS() {
+      super();
+      frontier = new Stack< Node >();
+    }
 
-		public Node getAndRemoveLeaf() {
+    public Node getAndRemoveLeaf() {
       // TODO: what if frontier is empty?
-			stackSize--;
-			return frontier.pop();
-		}
+      stackSize--;
+      return frontier.pop();
+    }
 
-		public void addToFrontier( Node n ) {
-			stackSize++;
+    public void addToFrontier( Node n ) {
+      stackSize++;
       frontier.push(n);
-		}
+    }
 
-		public int countFrontier() {
-			return stackSize;
-		}
+    public int countFrontier() {
+      return stackSize;
+    }
 
-		public boolean frontierIsEmpty() {
-			return (stackSize == 0) ? true : false;
-		}
+    public boolean frontierIsEmpty() {
+      return (stackSize == 0) ? true : false;
+    }
 
-		public boolean inFrontier( Node n ) {
-			return (frontier.search(n) == -1) ? false : true;
-		}
+    public boolean inFrontier( Node n ) {
+      return (frontier.search(n) == -1) ? false : true;
+    }
 
-		public String toString() {
-			return "Depth-first Search";
-		}
-	}
+    public String toString() {
+      return "Depth-first Search";
+    }
+  }
 
-	// Ex 3: Best-first Search uses a priority queue (Java contains no implementation of a Heap data structure)
-	public static class StrategyBestFirst extends Strategy {
-		private Heuristic heuristic;
-		public StrategyBestFirst( Heuristic h ) {
-			super();
-			heuristic = h;
-			// Unimplemented
-		}
-		public Node getAndRemoveLeaf() {
-			// Unimplemented
-			return null;
-		}
+  // Ex 3: Best-first Search uses a priority queue (Java contains no implementation of a Heap data structure)
+  public static class StrategyBestFirst extends Strategy {
+    private Heuristic heuristic;
+    private int initCapacity = 10;
+    private PriorityQueue< Node > frontier;
 
-		public void addToFrontier( Node n ) {
-			// Unimplemented
-		}
+    public StrategyBestFirst( Heuristic h ) {
+      super();
+      heuristic = h;
 
-		public int countFrontier() {
-			// Unimplemented
-			return 0;
-		}
+      frontier = new PriorityQueue< Node >(initCapacity, heuristic);
+    }
+    public Node getAndRemoveLeaf() {
+      return frontier.poll();
+    }
 
-		public boolean frontierIsEmpty() {
-			// Unimplemented
-			return true;
-		}
+    public void addToFrontier( Node n ) {
+      frontier.add(n);
+    }
 
-		public boolean inFrontier( Node n ) {
-			// Unimplemented
-			return false;
-		}
+    public int countFrontier() {
+      return frontier.size();
+    }
 
-		public String toString() {
-			return "Best-first Search (PriorityQueue) using " + heuristic.toString();
-		}
-	}
+    public boolean frontierIsEmpty() {
+      return (frontier.size() == 0) ? true : false;
+    }
+
+    public boolean inFrontier( Node n ) {
+      return frontier.contains(n);
+    }
+
+    public String toString() {
+      return "Best-first Search (PriorityQueue) using " + heuristic.toString();
+    }
+  }
 }
