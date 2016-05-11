9290c9d2dc57a7a34fbd031d58867929211cad03
Elias Obeid <e.k.obeid@gmail.com>
2016-05-11 23:02:20 +0200

    update client to send step-wise movements

--- project/client.py
+++ project/client.py
@@ -1,74 +1,77 @@
-
-#######   Imports   ########
-from parselvl import parselvl
-from high_level_plan import *
-from convert_path_to_moves import *
-from simple_grid import SimpleGrid
 import sys
 
-####### Parse Level ########
-(walls, goals, agents, boxes, colors, free) = parselvl()
+from parselvl import parselvl
+from high_level_plan import HighLevelPlan
+from convert_path_to_moves import calculate_movements_new
+from simple_grid import SimpleGrid
 
-####### Create Grid ########
-grid = SimpleGrid(set(walls), goals, boxes, agents, colors, set(free))
+# server responses
+TRUE, FALSE = 'true', 'false'
 
-####### High Level Plan ########
-hlp = HighLevelPlan(grid)
+def order_agents():
+    # create list with correct length
+    l = [0] * len(grid.agents)
+    # add agents in correct order
+    for agent in grid.agents:
+        idx = int(agent.name)
+        l[idx] = agent
+    return len(l), l
+
+def inform(string, end=None):
+    """ Use this function to for debugging """
+    end = end or "\n"
+    print(string, end=end, file=sys.stderr)
+
+def setup():
+    (walls, goals, agents, boxes, colors, free) = parselvl()
+    grid = SimpleGrid(set(walls), goals, boxes, agents, colors, set(free))
+    hlp = HighLevelPlan(grid)
+    return grid, hlp
+
+def align_movements():
+    longest_sequence = max(MOVES.values(), key=len)
+    for i, moves in MOVES.items():
+        if len(moves) == len(longest_sequence): continue
+        padding = len(longest_sequence) - len(moves)
+        MOVES[i] = moves + (["NoOp"] * padding)
+
+def next_move():
+    """ Construct next move """
+    s = [MOVES[i].pop(0) for i in range(NUM_AGENTS)]
+    a = ",".join(s)
+    return "[" + a + "]"
+
+# parse level from server, setup grid and planner
+grid, hlp = setup()
+# setup agents
+NUM_AGENTS, AGENTS = order_agents()
+# moves
+MOVES = dict()
+
+# testing debugging prints
+inform("write to stderr, without server doing anything")
+for a in AGENTS:
+    inform("{0} ({1}) at {2}".format(a.name, a.color, a.position))
+
+# initial plan
 hlp.find_shortest_box_goal_combination()
 hlp.create_paths()
+# debug agents' plans
+for a, p in hlp.agent_movement.items():
+    inform(a.name + " " + str(p))
 
+# convert to moves
+for idx in range(NUM_AGENTS):
+    MOVES[idx] = calculate_movements_new(hlp.agent_movement[AGENTS[idx]], grid)
 
-allSteps = []
-for key in hlp.agent_movement:
-    allSteps.append((key.name,hlp.agent_movement[key]))
+align_movements()
 
+while True:
+    server_response = input()
+    inform("server response: " + server_response)
 
-allAgentMoves = {}
-for (agent,steps) in allSteps:
-    agentMoves = calculate_movements(steps, grid)
-    allAgentMoves[int(agent)] = agentMoves
+    if len(MOVES[0]) > 0:
+        move = next_move()
 
-
-movesToPrint = ''
-
-""" Makes print string for server stuff """
-for move_index, _ in enumerate(allAgentMoves[0]):
-    movesToPrint = movesToPrint + '['
-    for agent_index,_ in enumerate(allSteps):
-        if move_index < len(allAgentMoves[agent_index]):
-            thisMove = allAgentMoves[agent_index][move_index]
-            thisMove = str(thisMove)
-            if not agent_index+1 in allAgentMoves:
-                movesToPrint = movesToPrint + thisMove
-            else:
-                movesToPrint = movesToPrint + thisMove +','
-        else:
-            break;
-    movesToPrint = movesToPrint + ']\n'
-
-
-
-
-# for i, move in enumerate(allAgentMoves[0]):
-#     for j, agent in enumerate(allAgentMoves):
-#         if j == 0:
-#             if len(allAgentMoves) == 1:
-#                 movesToPrint = movesToPrint + '[' + str(agent[i])+']\n'
-#             else:
-#                 movesToPrint = movesToPrint + '[' + str(agent[i])+','
-#         elif j < len(allAgentMoves) -1 :
-#             movesToPrint = movesToPrint + str(agent[i])+','
-#         else:
-#             movesToPrint = movesToPrint + str(agent[i])+']\n'
-#
-
-
-
-#movesToPrint = movesToPrint + '['+str(move)+']\n'
-
-sys.stdout.write(movesToPrint)
-sys.stdout.flush()
-sys.stderr.write(str(allAgentMoves[0]))
-sys.stderr.flush()
-
-
+        inform(move) # debug
+        print(move)  # send to server
